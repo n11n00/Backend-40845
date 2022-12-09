16e0ea519611dc460631dc7c2af0324789d886f8
unknown <nino.f.martinez@gmail.com>
2022-12-08 22:07:15 -0300

    nuevo commit

--- Desafios/desafio4/main.js
+++ Desafios/desafio4/main.js
@@ -1,6 +1,4 @@
 // >> Consigna: Realizar un proyecto de servidor basado en node.js y express que ofrezca una API RESTful de productos. En detalle, que incorpore las siguientes rutas:
-
-
 // GET '/api/productos' -> devuelve todos los productos.
 // GET '/api/productos/:id' -> devuelve un producto según su id.
 // POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
@@ -8,77 +6,100 @@
 // DELETE '/api/productos/:id' -> elimina un producto según su id.
 
 
+const express = require('express');
 
-// Cada ítem almacenado dispondrá de un id numérico proporcionado por el backend, comenzando en 1, y que se irá incrementando a medida de que se incorporen productos. Ese id será utilizado para identificar un producto que va a ser listado en forma individual.
-// Para el caso de que un producto no exista, se devolverá el objeto:
-// { error : 'producto no encontrado' }
-// Implementar la API en una clase separada, utilizando un array como soporte de persistencia en memoria.
-// Incorporar el Router de express en la url base '/api/productos' y configurar todas las subrutas en base a este.
-// Crear un espacio público de servidor que contenga un documento index.html con un formulario de ingreso de productos con los datos apropiados.
-// El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080 y en caso de error, representar la descripción del mismo.
-// Las respuestas del servidor serán en formato JSON. La funcionalidad será probada a través de Postman y del formulario de ingreso.
+class Contenedor {
+  constructor() {
+    this.id = 0;
+    this.content = [];
+  }
+
+  save = (item) => {
+    item.id = this.id;
+    this.content.push(item);
+    this.id ++;
+
+    return item.id;
+  }
+
+// Devuelve los productos segun su id.
+
+  getById = (id) => {
+    const item = this.content.find(element => element.id === id);
+    return item ? item : null;
+  }
+
+// Devuelve todos los productos dentro del array.
+
+  getAll = () => this.content;
 
 
-const express = require ('express');
+//Borra el producto segun su id.
+
+  deleteById = (id) => {
+    const updatedContent = this.content.filter(element => element.id !== id);
+    this.content = updatedContent;
+  }
+
+//Borra todos los productos del array
+
+  deleteAll = () => {
+    this.content = [];
+  }
+}
+
 const app = express();
-const { Router } = express;
-const productsRouter = Router()
-const PORT = 3000
+const productsRouter = express.Router();
+
+
 app.use(express.json());
-app.use(`/api` , productsRouter);
+
+const productClass = new Contenedor();
+
+//GET   devuelve todos los productos.
+productsRouter.get('/productos', (req, res) => {
+  const products = productClass.getAll();
+
+  res.json(products);
+});
+
+//GET  devuelve un producto según su id.
+productsRouter.get('/productos/:id', (req, res) => {
+  const productId = req.params.id;
+  const products = productClass.getById(Number(productId));
+
+  res.json(products);
+});
+
+//POST  recibe y agrega un producto, y lo devuelve con su id asignado.
+productsRouter.post('/productos', (req, res) => {
+  const product = req.body;
+  const id = productClass.save(product);
+
+  res.send(id.toString());
+});
 
 
 
+// PUT  recibe y actualiza un producto según su id.
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor Http escuchando en el puerto ${server.address().port}`)    
+ productsRouter.put('/productos/:id', (req, res) => {
+    const productId = req.params.id;
+     const products = productClass.splice(Number(productId, 1));
+  
+     res.json(products);
+   });
+
+
+//DELETE '/api/productos/:id' -> elimina un producto según su id.
+
+productsRouter.delete('/productos/:id', (req, res) => {
+    const productId = req.params.id;
+    const deleteProductById = productClass.deleteById(productId);
+  
+    res.json(productClass);
   });
 
-server.on("error", error => console.log(`Error en servidor${error}`));
-  
+app.use('/api', productsRouter);
 
-
-// GET '/api/productos'
-
-const fs = require ('fs')
-
-productsRouter.get('/productos', function (req, res) {
-  const objects = fs.readFileSync('products.txt', 'utf-8');
-  parseObjects = JSON.parse(objects);
-  res.json({parseObjects});
-});
-
-
-
-
-
-
-// GET '/api/productos/:id'
-
-// productsRouter.get('/productos/:id', (req, res) => {
-//   const inyectID = fs.writeFileSync('products.txt', 'a');
-
-  // const id = req.params.id;
-
-  // res.json({ parseObjects:  });
-// })
-
-
-
-
-
- 
-
-// POST
-const product = [];
-
-productsRouter.post('/productos', (req, res) => {
-    const addProduct = req.body;
-    product.push(addProduct);
-
-    res.json({ product });
-
-    const productsInArray = product.length;
-    console.log(productsInArray)
-});
-
+app.listen(8080);
